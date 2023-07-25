6eaf273b55294a197d82f29fa3ed456cf1da087b
MishMarchenko <mishmar4enko@gmail.com>
2023-07-25 21:51:50 +0300

    started

--- src/test/java/NewMain.java
+++ src/test/java/NewMain.java
@@ -4,5 +4,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class NewMain {
     public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://ru.wikipedia.org/");
+        driver.manage().window().maximize();
+        try{
+            Thread.sleep(5000);
+        } catch (InterruptedException e){
+            throw new RuntimeException(e);
+        }
+        driver.quit();
     }
 }
