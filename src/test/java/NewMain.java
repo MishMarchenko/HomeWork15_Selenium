f1b007a16e21a1afa28ffeedb2e2e9e41e36eb19
MishMarchenko <mishmar4enko@gmail.com>
2023-07-25 21:49:40 +0300

    init commit

---
+++ src/test/java/NewMain.java
@@ -0,0 +1,8 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NewMain {
+    public static void main(String[] args) {
+    }
+}
